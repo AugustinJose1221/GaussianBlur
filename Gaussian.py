c12ff52f9fccfb7cad9dc7c66c3d3681b120cb77
AugustinJose1221 <augustinjose1221@gmail.com>
2020-06-05 19:05:28 +0530

    Minor Updates

--- Gaussian.py
+++ Gaussian.py
@@ -23,3 +23,34 @@ def openImage(image):
     image = Image.open(image)
     image = np.asarray(image)
     return image
+
+def blur(image, level=5):
+    gaussian = kernal(6, level)
+    frame = openImage(image)
+    height = len(frame)
+    width = len(frame[0])
+    conv = np.tile(0, 3)
+    padding = int(level/2)
+    blur_frame = np.tile(0, (height, width, 3))
+    final_frame = np.tile(0, (height, width, 3))
+    for i in range(height-len(gaussian)):
+        for j in range(width-len(gaussian)):
+            for k in range(len(gaussian)):
+                conv = conv + frame[i][j+k]*gaussian[k]
+            blur_frame[i][j] = conv
+            conv = np.tile(0, 3)
+    conv = np.tile(0, 3)
+    for i in range(width-len(gaussian)):
+        for j in range(height-len(gaussian)):
+            for k in range(len(gaussian)):
+                conv = conv + blur_frame[j+k][i]*gaussian[k]
+            final_frame[j][i] = conv
+            conv = np.tile(0, 3)
+    
+    img = Image.fromarray(final_frame, 'RGB')
+    img.save('Output/Blur1.jpg')
+    img.show()
+    return True
+
+blur("img/test2.jpg")
+print("Done")
